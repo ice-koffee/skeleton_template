2759557e01d80a4239c270f96fe54092bf35683a
Isioma <isiomaekejiuba@gmail.com>
2023-05-03 19:33:20 +0000

    db credentials for online

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,16 @@
 <?php
 //Database credentials
-define("DATABASE",  "reconnect");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+$db='heroku_744a0530e414c06';
+$server='us-cdbr-east-06.cleardb.net';
+$username='bc4055468504d3';
+$passwd='17989265';
+
+
+
+
+define("DATABASE",  $db);
+define("SERVER", $server);
+define("USERNAME", $username);
+define("PASSWD", $passwd);
 
 ?>
